5d781a3312727a6d7c670c3e57f9a4113ce60754
Bill Chan <billypchan@gmail.com>
2020-11-18 16:22:24 +0100

    Update checkoutReleaseTag.sh

--- scripts/checkoutReleaseTag.sh
+++ scripts/checkoutReleaseTag.sh
@@ -5,11 +5,13 @@
 
 UPSTREAM_REPO=$1
 
+echo "ℹ️ upstream repo is: $UPSTREAM_REPO"
+
 LATEST_VERSION=`curl --silent "https://api.github.com/repos/$UPSTREAM_REPO/releases/latest" | jq -r .tag_name` || true
 
 echo "ℹ️ upstream latest version is $LATEST_VERSION"
 
-echo "ℹ️ creating release"
+echo "ℹ️ checkout release tag"
 
 git checkout tags/$LATEST_VERSION
 
