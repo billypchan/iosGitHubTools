ca700a5095077489692ae4595dbc0762c77e05cb
bill chan <billypchan@gmail.com>
2020-11-18 10:30:53 +0100

    fix

--- scripts/updateFork.sh
+++ scripts/updateFork.sh
@@ -2,7 +2,7 @@
 ## updateFork.sh [upstream repo]
 ## e.g. updateFork.sh https://github.com/apple/swift-protobuf
 
-git remote add $1
+git remote add upstream $1
 git fetch upstream
 git checkout master
 git merge upstream/master
