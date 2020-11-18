60e2e0f88626f8de6c5a34b1ae184c31b77a40d5
bill chan <billypchan@gmail.com>
2020-11-18 10:34:11 +0100

    echo

--- scripts/updateFork.sh
+++ scripts/updateFork.sh
@@ -2,6 +2,8 @@
 ## updateFork.sh [upstream repo]
 ## e.g. updateFork.sh https://github.com/apple/swift-protobuf
 
+echo "add upstream repo: $1"
+
 git remote add upstream $1
 git fetch upstream
 git checkout master
