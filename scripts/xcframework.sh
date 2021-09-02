85803f99f5c716a396a467d88a1d95df5a11bd53
Bill Chan <billypchan@gmail.com>
2021-09-02 10:48:45 +0200

    add xcframework creation script

---
+++ scripts/xcframework.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# exit when any command fails
+set -e
+
+### usage: xcframework.sh [xcodeproj filename] [scheme] [framework name]
+### e.g. xcframework.sh ZipArchive ZipArchive-iOS ZipArchive
+
+
+if [ $# -ne 3 ]
+  then
+    echo "No arguments supplied"
+    echo "usage: xcframework.sh [xcodeproj filename] [scheme] [framework name]"
+    exit
+fi
+
+xcodeproj=$1
+scheme=$2
+framework=$3
+
+xcodebuild archive -project "$xcodeproj.xcodeproj" \
+-scheme "$scheme" \
+-configuration Release -destination 'generic/platform=iOS' \
+-archivePath "./$xcodeproj.framework-iphoneos.xcarchive" \
+SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+
+xcodebuild archive -project "$xcodeproj.xcodeproj" -scheme "$scheme" -configuration Release -destination 'generic/platform=iOS Simulator' -archivePath "./$xcodeproj.framework-iphonesimulator.xcarchive" SKIP_INSTALL=NO  BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+
+xcodebuild -create-xcframework -framework \
+"./$xcodeproj.framework-iphoneos.xcarchive/Products/Library/Frameworks/$framework.framework" \
+-framework "./$xcodeproj.framework-iphonesimulator.xcarchive/Products/Library/Frameworks/$framework.framework" \
+-output "$framework.xcframework"
+
+zip -r $framework.xcframework.zip "$framework.xcframework"
