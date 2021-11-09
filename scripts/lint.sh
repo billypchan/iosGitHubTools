6b4313ade059f2f4e40f7092bc634efd09432f35
Bill Chan <billypchan@gmail.com>
2021-11-09 15:52:07 +0100

    update

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,7 +2,7 @@ git diff develop --name-only | while read filename; do
 
 if [[ $filename == *.swift ]] 
 then
-	swiftlint autocorrect --path "$filename";
+	swiftlint --fix --path "$filename";
 fi
 
 done
